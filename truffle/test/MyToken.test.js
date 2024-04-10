0e0cb83e121413563c07e41396555cfc4d1d3259
Jonathan Nascimento <nascimento.jonathan@live.com>
2024-04-10 15:06:49 -0300

    finalizacao de testes de erc20

--- truffle/test/MyToken.test.js
+++ truffle/test/MyToken.test.js
@@ -1,22 +1,48 @@
 const Token = artifacts.require("MyToken");
+const { BN } = require("web3-utils"); // Adicionando importação para BN
 
 var chai = require("chai");
-const BigNumber = require("bignumber.js");
-const BN = web3.utils.BN;
-const chaiBN = require("chai-bignumber")(BigNumber);
-chai.use(chaiBN);
-
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
-
+chai.use(require("chai-as-promised"));
 const expect = chai.expect;
 
 contract("Token Test", async (accounts) => {
+    const [deployerAccount, recipient] = accounts;
 
-   it("all tokens should be in my account", async () => {
-      let instance = await Token.deployed();
-      let totalSupply = await instance.totalSupply();
-      expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply);
-   })
+    it("all tokens should be in my account", async () => {
+        let instance = await Token.deployed();
+        let totalSupply = await instance.totalSupply();
+        let balance = await instance.balanceOf(deployerAccount);
+        expect(balance.toString()).to.equal(totalSupply.toString());
+    });
 
+    it("is possible to send tokens between accounts", async () => {
+        const sendTokens = 1;
+        let instance = await Token.deployed();
+        let totalSupply = await instance.totalSupply();
+        let initialSenderBalance = await instance.balanceOf(deployerAccount);
+        let initialRecipientBalance = await instance.balanceOf(recipient);
+
+        // Realiza a transferência de tokens
+        await instance.transfer(recipient, sendTokens);
+
+        // Verifica os saldos após a transferência
+        let finalSenderBalance = await instance.balanceOf(deployerAccount);
+        let finalRecipientBalance = await instance.balanceOf(recipient);
+
+        // Verifica se os saldos foram ajustados corretamente
+        expect(finalSenderBalance.toNumber()).to.equal(initialSenderBalance.toNumber() - sendTokens);
+        expect(finalRecipientBalance.toNumber()).to.equal(initialRecipientBalance.toNumber() + sendTokens);
+    });
+
+    it("não é possível enviar mais tokens do que o disponível no total de tokens", async () => {
+        let instance = await Token.deployed();
+        let balanceOfDeployer = await instance.balanceOf(deployerAccount);
+
+        // Tenta transferir mais tokens do que o disponível no saldo do remetente
+        await expect(instance.transfer(recipient, balanceOfDeployer.add(new BN(1)))).to.be.rejected;
+
+        // Verifica se o saldo do remetente permanece inalterado
+        let finalBalanceOfDeployer = await instance.balanceOf(deployerAccount);
+        expect(finalBalanceOfDeployer.toString()).to.equal(balanceOfDeployer.toString());
+    });
 });
