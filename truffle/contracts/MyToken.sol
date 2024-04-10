ff47e4c2b8ab166e8e0e4a4e3fec754ebaa2bff7
Jonathan Nascimento <nascimento.jonathan@live.com>
2024-04-10 11:00:09 -0300

    implementacao de testes unitarios

--- truffle/contracts/MyToken.sol
+++ truffle/contracts/MyToken.sol
@@ -1,9 +1,11 @@
-pragma solidity 0.6.1;
+// SPDX-License-Identifier: MIT
+//contracts/MyToken.sol
+pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("StarDucks Cappucino Token", "CAPPU", 0) {
+    constructor(uint256 initialSupply) ERC20("StarDucks Cappucino Token", "CAPPU") {
         _mint(msg.sender, initialSupply);
     }
 }
---
+++ truffle/test/MyToken.test.js
@@ -0,0 +1,22 @@
+const Token = artifacts.require("MyToken");
+
+var chai = require("chai");
+const BigNumber = require("bignumber.js");
+const BN = web3.utils.BN;
+const chaiBN = require("chai-bignumber")(BigNumber);
+chai.use(chaiBN);
+
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
+const expect = chai.expect;
+
+contract("Token Test", async (accounts) => {
+
+   it("all tokens should be in my account", async () => {
+      let instance = await Token.deployed();
+      let totalSupply = await instance.totalSupply();
+      expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(totalSupply);
+   })
+
+});
