c8a0764e2be957d72472c40b05d7bf5c880da912
Jonathan Nascimento <nascimento.jonathan@live.com>
2024-04-15 17:04:27 -0300

    merge

--- truffle/test/MyTokenSale.test.js
+++ truffle/test/MyTokenSale.test.js
@@ -2,6 +2,7 @@ const Token = artifacts.require("MyToken");
 const TokenSale = artifacts.require("MyTokenSale");
 const { BN } = require("bn.js");
 
+
 const chai = require("./setupchai.js");
 const expect = chai.expect;
 
@@ -13,7 +14,7 @@ contract("TokenSale Test", async (accounts) => {
 
     it('should not have any tokens in my deployerAccount', async () => {
       let instance = await Token.deployed();
-      await expect(await instance.balanceOf(deployerAccount)).to.be.a.bignumber.equal(new BN(0));
+      return expect(await instance.balanceOf(deployerAccount)).to.be.a.bignumber.equal(new BN(0));
     });
 
     it('all tokens should be in the TokenSale Smart Contract by default', async() => {
@@ -23,8 +24,13 @@ contract("TokenSale Test", async (accounts) => {
       expect(balanceOfTokenSaleSmartContract).to.be.a.bignumber.equal(totalSupply);
     });
 
-    
-    
+    // it('should be possible to buy tokens', async () => {
+    //   let tokenInstance = await Token.deployed();
+    //   let tokenSaleInstance = await TokenSale.deployed();
+    //   let balanceBefore = await tokenInstance.balanceOf(deployerAccount);
+    //   expect(tokenSaleInstance.sendTransaction({from: deployerAccount, value: web3.utils.toWei("1","wei")})).to.be.fulfilled;
+    //   return expect(tokenInstance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceBefore.add(new BN(1)));
+    // });
     
 });
 
