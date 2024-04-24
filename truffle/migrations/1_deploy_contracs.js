663770467683dc669d16968dd516c186bfbe7b91
Jonathan Nascimento <nascimento.jonathan@live.com>
2024-04-24 11:17:50 -0300

    finalizacao tokenization

--- truffle/migrations/1_deploy_contracs.js
+++ truffle/migrations/1_deploy_contracs.js
@@ -1,6 +1,6 @@
 var MyToken = artifacts.require("MyToken.sol");
 var MyTokenSale = artifacts.require("MyTokenSale");
-var MyKycContract = artifacts.require("MyKycContract");
+var KycContract = artifacts.require("KycContract");
 require("dotenv").config({path:"../.env"});
 console.log(process.env);
 
@@ -8,10 +8,12 @@ module.exports = async function(deployer) {
 
    let addr = await web3.eth.getAccounts();
 
+   const ownerAddress = addr[0];
+
    await deployer.deploy(MyToken, process.env.INITIAL_TOKENS);
-   await deployer.deploy(MyKycContract);
-   
-   await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address, MyKycContract.address);
+   await deployer.deploy(KycContract, ownerAddress);
+
+   await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address, KycContract.address);
    
    let instance = await MyToken.deployed();
    await instance.transfer(MyTokenSale.address, process.env.INITIAL_TOKENS);
